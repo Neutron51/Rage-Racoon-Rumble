47de0c601d61563be1f44f87e1488ea39052d939
Nacho <neutron.puntahome@gmail.com>
2026-04-14 15:40:25 +0300

    Player anim

--- Assets/Scripts/Player/PlayerDirection.cs
+++ Assets/Scripts/Player/PlayerDirection.cs
@@ -8,8 +8,6 @@ public class PlayerDirection : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        //Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
-
         if(Physics.Raycast(ray, out hit))
         {
             Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
