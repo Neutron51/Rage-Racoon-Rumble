6cc7708d52e256d284b76a4d84d605f11158d81f
Kimathi Universe <kxmatree@gmail.com>
2026-04-23 10:15:11 +0300

    Props additions MVP

---
+++ Assets/Scripts/KOKEILU/DropDownScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class ScenePhysicsTool : MonoBehaviour {
+
+    /* public static SimulationMode simulationMode;
+
+     private void OnGUI()
+     {
+         if (GUILayout.Button("Run Physics"))
+         {
+             StepPhysics();
+         }
+     }
+
+     private void StepPhysics()
+     {
+         Physics.simulationMode = false;
+         Physics.Simulate(Time.fixedDeltaTime);
+         Physics.simulationMode = true;
+     }
+
+     [MenuItem("Tools/Scene Physics")]
+     private static void OpenWindow()
+     {
+         GetWindow<ScenePhysicsTool>(false, "Physics", true);
+     } */
+}
